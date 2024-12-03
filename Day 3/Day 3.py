bafe60debd4c5581a33e9c16a09d8f78de0fbdc8
Log <loganhaywood1605@gmail.com>
2024-12-03 22:27:49 +0000

    Day 3

---
+++ Day 3/Day 3.py
@@ -0,0 +1,27 @@
+import re
+
+def data():
+    with open("Day 3/Day3-Data.txt","r") as f: return f.read()
+
+def part1(data=data(),score=0):
+    for item in re.findall(r"mul\(\d\d?\d?,\d\d?\d?\)",data):
+        numlist=re.findall(r"\d\d?\d?",item)
+        score+=int(numlist[0])*int(numlist[1])
+    return score
+
+def part2(data=data(),score=0):
+    do=True
+    for item in re.findall(r"mul\(\d\d?\d?,\d\d?\d?\)|do\(\)|don't\(\)",data):
+        if item=="do()":
+            do=True
+            next
+        elif item=="don't()":
+            do=False
+            next
+        elif do:
+            numlist=re.findall(r"\d\d?\d?",item)
+            score+=int(numlist[0])*int(numlist[1])
+    return score
+
+print(part1())
+print(part2())
